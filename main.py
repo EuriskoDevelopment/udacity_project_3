5861245ae295d7c87d9d30a59de68afc8dd086c1
EuriskoDeveloment <re@pandema.com>
2022-01-17 20:13:59 +0100

    Try to get more error info from Heroku

--- main.py
+++ main.py
@@ -11,14 +11,19 @@ from sklearn.model_selection import train_test_split
 from starter.starter.ml.data import process_data
 import pandas as pd
 import os
+import subprocess
 
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
     os.system("dvc remote add -df s3-bucket s3://euriskoudacityproject3")
     print("AWS set up")
-    if os.system("dvc pull") != 0:
-        exit("dvc pull failed")
+    dvc_output = subprocess.run(
+    ["dvc", "pull"], capture_output=True, text=True)
+    print(dvc_output.stdout)
+    print(dvc_output.stderr)
+    if dvc_output.returncode != 0:
+        print("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 
